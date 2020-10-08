dbfc8194c5be678c9573576ba38c934d514353b8
maalexra <maalexra@uark.edu>
2020-10-08 00:10:27 -0500

    Testing task 4

--- src/main/java/edu/uark/registerapp/controllers/ProductDetailRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductDetailRouteController.java
@@ -16,7 +16,7 @@ import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.Product;
 
 @Controller
-@RequestMapping(value = "/")
+@RequestMapping(value = "/productDetail")
 public class ProductDetailRouteController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView start() {
