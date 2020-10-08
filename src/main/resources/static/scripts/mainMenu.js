ba49114e8e4526b8305a289f2baf3d43410f8ea2
Carson <ctpartee@email.uark.edu>
2020-10-08 14:33:10 -0500

    Added MainMenu functionality

---
+++ src/main/resources/static/scripts/mainMenu.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("startTransactionButton").addEventListener("click", startTransactionClick);
+    document.getElementById("productListingButton").addEventListener("click", productListingClick);
+    document.getElementById("employeeDetailsButton").addEventListener("click", employeeDetailsClick);
+    document.getElementById("salesReportButton").addEventListener("click", salesReportClick);
+    document.getElementById("cashierReportButton").addEventListener("click", cashierReportClick);
+});
+
+function startTransactionClick(event) {
+    displayError("Functionality has not yet been implemented.");
+}
+
+function productListingClick(event) {
+    window.location.assign("/productListing/");
+}
+
+function employeeDetailsClick(event) {
+    window.location.assign("/employeeDetail/");
+}
+
+function salesReportClick(event) {
+    displayError("Functionality has not yet been implemented.");
+}
+
+function cashierReportClick(event) {
+    displayError("Functionality has not yet been implemented.");
+}
